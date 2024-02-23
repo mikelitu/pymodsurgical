3efdd8183a5eecf73359687f6f9baed42096e067
mikelitu <mikelitubiomed@gmail.com>
2024-02-23 13:06:32 +0000

    refactor the coding style and spacing

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 from pathlib import Path, PosixPath
 
 
-def create_save_dir(save_dir: PosixPath | str, filename: str) -> PosixPath:
+def create_save_dir(
+    save_dir: PosixPath | str, 
+    filename: str,
+) -> PosixPath:
     if not isinstance(save_dir, PosixPath):
         save_dir = Path(save_dir)
     if not save_dir.exists():
