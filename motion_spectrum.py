836c39812aa56d6088f877c256ead12c5fbf85ce
mikelitu <mikelitubiomed@gmail.com>
2024-02-23 13:06:05 +0000

    update the saving function for stereo spectrums

--- motion_spectrum.py
+++ motion_spectrum.py
@@ -5,15 +5,19 @@ import numpy as np
 from utils import create_save_dir
 from filtering import GaussianFiltering
 from masking import Masking
+from video_writer import VideoWriter
 
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 model = load_flow_model(device)
 
 def calculate_motion_spectrum(
     frames: list[np.ndarray], 
-    K: int, batch_size: int = 500, 
+    K: int, 
+    batch_size: int = 500, 
     filtered: bool = True,
-    mask: Masking | None = None
+    mask: Masking | None = None,
+    camera_pos: str | None = None,
+    save_flow_video: bool = False
 ) -> torch.Tensor:
     
     preprocess_frames = [preprocess_for_raft(frame) for frame in frames]
@@ -36,27 +40,46 @@ def calculate_motion_spectrum(
     if filtered:
         filtering = GaussianFiltering((3, 3), 0.5)
         flows = filtering(preprocess_frames[1:], flows)
-    
 
+    if save_flow_video:
+        video_writer = VideoWriter("flows.mp4", {"fps": 30}, "mono")
+        video_writer(flows)
 
     motion_spectrum = motion_texture_from_flow_field(flows, K, flows.shape[0]).squeeze(0)
     
     if mask is not None:
-        motion_spectrum = mask(motion_spectrum)
+        motion_spectrum = mask(motion_spectrum, camera_pos=camera_pos)
         
     return motion_spectrum.detach().cpu()
 
 
-def save_motion_spectrum(motion_spectrum: torch.Tensor | tuple[torch.Tensor, torch.Tensor], save_dir: PosixPath | str) -> None:
+def save_motion_spectrum(
+    motion_spectrum: torch.Tensor | tuple[torch.Tensor, torch.Tensor],
+    save_dir: PosixPath | str,
+    filtered: bool = False,
+    masked: bool = False
+) -> None:
+    
     if isinstance(motion_spectrum, tuple):
         filenames = {0: "left_motion_spectrum.png", 1: "right_motion_spectrum.png"}
         for i in range(len(motion_spectrum)):
-            tmp_save_dir = create_save_dir(save_dir, filenames[i])
+            filename = filenames[i]
+            if filtered:
+                filename = "filtered_" + filename
+            if masked:
+                filename = "masked_" + filename
+                
+            tmp_save_dir = create_save_dir(save_dir, filename)
             print(f"Saving motion spectrum to: {tmp_save_dir}")
             img_motion_spectrum_X, img_motion_spectrum_Y = motion_spectrum_2_grayimage(motion_spectrum[i])
-            plot_and_save([img_motion_spectrum_X, img_motion_spectrum_Y], tmp_save_dir, cmap="RdPu")
+            plot_and_save([img_motion_spectrum_X, img_motion_spectrum_Y], tmp_save_dir, cmap="plasma")
     else:
-        save_dir = create_save_dir(save_dir, "motion_spectrum.png")
+        filename = "motion_spectrum.png"
+        if filtered:
+            filename = "filtered_" + filename
+        if masked:
+            filename = "masked_" + filename
+        save_dir = create_save_dir(save_dir, filename)
         print("Saving motion spectrum to: ", save_dir)
         img_motion_spectrum_X, img_motion_spectrum_Y = motion_spectrum_2_grayimage(motion_spectrum)
-        plot_and_save([img_motion_spectrum_X, img_motion_spectrum_Y], save_dir, cmap="RdPu")
+        plot_and_save([img_motion_spectrum_X, img_motion_spectrum_Y], save_dir, cmap="plasma")
