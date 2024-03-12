7c13d3e333180247c52ffe713a7e1f137f91745a
mikelitu <mikelitubiomed@gmail.com>
2024-03-12 17:31:46 +0000

    update the

--- complex.py
+++ complex.py
@@ -38,11 +38,14 @@ def motion_spectrum_2_complex(
         right_complex_motion_spectrum = motion_spectrum_2_complex(right_motion_spectrum)
         return left_complex_motion_spectrum, right_complex_motion_spectrum
     
-    K, _, height, width = motion_spectrum.shape
-    complex_motion_spectrum = torch.zeros(K, 2, height, width).to(motion_spectrum.device, dtype=torch.cfloat)
+    K, dim, height, width = motion_spectrum.shape
+    complex_motion_spectrum = torch.zeros(K, 2 if dim==4 else 3, height, width).to(motion_spectrum.device, dtype=torch.cfloat)
 
     complex_motion_spectrum[:, 0, :, :] = motion_spectrum[:, 0, :, :] + 1j * motion_spectrum[:, 1, :, :]
     complex_motion_spectrum[:, 1, :, :] = motion_spectrum[:, 2, :, :] + 1j * motion_spectrum[:, 3, :, :]
+    if dim == 6:
+        complex_motion_spectrum[:, 2, :, :] = motion_spectrum[:, 4, :, :] + 1j * motion_spectrum[:, 5, :, :]
+    
     return complex_motion_spectrum
 
 def normalize_modal_coordinate(
@@ -57,7 +60,10 @@ def normalize_modal_coordinate(
 def plot_modal_coordinates(
     modal_coordinates: torch.Tensor | np.ndarray,
     displacement: torch.Tensor | None = None,
-    pixel: tuple[int, int] | None = None
+    pixel: tuple[int, int] | None = None,
+    show: bool = True,
+    save: bool = False,
+    save_dir: str | PosixPath = "./figures"
 ) -> None:
     """
     Plot the modal coordinates.
@@ -68,40 +74,53 @@ def plot_modal_coordinates(
         modal_coordinates = normalize_modal_coordinate(modal_coordinates)
         modal_coordinates = modal_coordinates.detach().cpu().numpy()
     
+    plot_names = ["X", "Y", "Z"]
+
     # Create the figure
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 10))
+    fig, axs = plt.subplots(ncols=modal_coordinates.shape[1], figsize=(16, 7))
     
-    # Setting the aspect ratio of the plot to 'equal to ensure scaled arrows
-    ax1.set_aspect('equal', 'box')
-    ax2.set_aspect('equal', 'box')
-
-    # Plot each modal coordinate
+    # Setting the properties of the plot
+    for i, ax in enumerate(axs):
+        ax.set_aspect('equal', 'box')
+        ax.set_xlim(-1.5, 1.5)
+        ax.set_ylim(-1.5, 1.5)
+        ax.grid(True)
+        ax.axhline(0, color='black', lw=0.5)
+        ax.axvline(0, color='black', lw=0.5)
+        ax.set_title(f'Modal Coordinates {plot_names[i]}')
+    
     for mc in modal_coordinates:
-        ax1.quiver(0, 0, mc[0].real, mc[0].imag, angles='xy', scale_units='xy', scale=1, color='r')
-        ax2.quiver(0, 0, mc[1].real, mc[1].imag, angles='xy', scale_units='xy', scale=1, color='r')
+        ax.set_aspect('equal', 'box')
+
+        # Plot each modal coordinate
+        for i, ax in enumerate(axs):
+            ax.quiver(0, 0, mc[i].real, mc[i].imag, angles='xy', scale_units='xy', scale=1, color='r')
+
+    # Set the title of the plot
+    if modal_coordinates.shape[1] == 2:
+        str_displacement = f"({displacement[0]}, {displacement[1]})"
+    else:
+        str_displacement = f"({displacement[0]}, {displacement[1]}, {displacement[2]})"
+
+    str_pixel = f"({pixel[0]}, {pixel[1]})"
+    title = f"Modal Coordinates for Displacement {str_displacement} and Pixel {str_pixel}"
+    fig.suptitle(title, fontsize=16)
+    fig.supylabel('Imaginary Part')
+    fig.supxlabel('Real Part')
+
+    if save:
+        if isinstance(save_dir, str):
+            save_dir = Path(save_dir)
+        save_dir.mkdir(exist_ok=True, parents=True)
+        filename = save_dir/f"modal_coordinates_{str_displacement}_{str_pixel}.png"
+        fig.tight_layout()
+        fig.savefig(filename, dpi=300)
+        print(f"Modal coordinates saved to: {filename}")
+
+    if show:
+        plt.show()
     
-    # Set the limits of the plot (we know all the coordinates are normalized)
-    ax1.set_xlim(-1.5, 1.5)
-    ax1.set_ylim(-1.5, 1.5)
-    ax1.grid(True)
-    ax1.axhline(0, color='black', lw=0.5)
-    ax1.axvline(0, color='black', lw=0.5)
-    ax1.set_xlabel('Real Part')
-    ax1.set_ylabel('Imaginary Part')
-    ax1.set_title('Modal Coordinates X')
-
-    # Set the labels of the plot
-    ax2.set_xlim(-1.5, 1.5)
-    ax2.set_ylim(-1.5, 1.5)
-    ax2.grid(True)
-    ax2.axhline(0, color='black', lw=0.5)
-    ax2.axvline(0, color='black', lw=0.5)
-    ax2.set_xlabel('Real Part')
-    ax2.set_ylabel('Imaginary Part')
-    ax2.set_title('Modal Coordinates Y')
-
-    fig.suptitle(f"Displacement: ({displacement[0]}, {displacement[1]}) Pixel: ({pixel[0]}, {pixel[1]})")
-    plt.show()
+    
 
 
 def save_modal_coordinates(
@@ -126,8 +145,14 @@ def save_modal_coordinates(
     save_dir = save_dir / "modal_coordinates"
     save_dir.mkdir(exist_ok=True, parents=True)
     
+    if displacement.shape[0] == 2:
+        filename = save_dir/f"disp_{round(displacement[0].real.item(), 2)}_{round(displacement[1].real.item(), 2)}_px_{pixel[0]}_{pixel[1]}.npy"
+    else:
+        filename = save_dir/f"disp_{round(displacement[0].real.item(), 2)}_{round(displacement[1].real.item(), 2)}_{round(displacement[2].real.item(), 2)}_px_{pixel[0]}_{pixel[1]}.npy"
+    
+    np.save(filename, modal_coordinates)
+    print(f"Modal coordinates saved to: {filename}")
 
-    np.save(save_dir/f"disp_{round(displacement[0].real.item(), 2)}_{round(displacement[1].real.item(), 2)}_px_{pixel[0]}_{pixel[1]}.npy", modal_coordinates)
 
 def load_modal_coordinates(
     path: str
