34c33ea2ec9995f9bb68e6578b6b44d9345e806b
mikelitu <mikelitubiomed@gmail.com>
2024-05-03 11:22:43 +0100

    add roi selector

--- src/pymodal_surgical/apps/force_estimator/force_estimator.py
+++ src/pymodal_surgical/apps/force_estimator/force_estimator.py
@@ -8,6 +8,7 @@ import pymodal_surgical.modal_analysis.optical_flow as optical_flow
 import pymodal_surgical.modal_analysis.force as force
 import pymodal_surgical.modal_analysis.math_helper as math_helper
 import matplotlib.pyplot as plt
+import cv2
 
 
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
@@ -29,30 +30,46 @@ class ForceEstimator():
     def _load_force_video(
         self,
         force_estimation_config: dict
-    ) -> VideoReader:
+    ) -> None:
         
         self.force_video_reader = VideoReader(video_config=force_estimation_config)
         self.flow_model = optical_flow.load_flow_model(device)
+        self.pixels = None
 
     
     def _show_frames(
         self,
         frame: np.ndarray,
     ) -> None:
-        plt.imshow(frame)
-        plt.show()
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(0)
+
+
+    def _select_roi(
+        self,
+        frame: np.ndarray,
+    ) -> tuple[int, int]:
         
+        frame = cv2.resize(frame, (self.mode_shapes.shape[3], self.mode_shapes.shape[2]))
+        bbox = cv2.selectROI("Frame", frame, fromCenter=True)
+        cv2.destroyAllWindows()
+        return bbox
     
-    def _calculate_force(
+    def calculate_force(
         self,
         idx_1: int,
         idx_2: int,
-        pixels: tuple[int, int],
     ) -> torch.Tensor:
         
         # Load the frames
         frame_1 = self.force_video_reader.read_frame(idx_1)
         frame_2 = self.force_video_reader.read_frame(idx_2)
+
+        if self.pixels is None:
+            roi = self._select_roi(frame_1)
+            pix_w = (roi[1], roi[1] + roi[3])
+            pix_h = (roi[0], roi[0] + roi[2])
+            self.pixels = (pix_w, pix_h)
 
         # Preprocess the frames for the calculation of optical flow
         frame_1 = optical_flow.preprocess_for_raft(frame_1).unsqueeze(0).to(device)
@@ -70,14 +87,13 @@ class ForceEstimator():
         # Calculate the constraint force
         modal_coordinates = torch.zeros(self.mode_shapes.shape[0], 2)
         norm_mode_shapes = math_helper.orthonormal_normalization(self.mode_shapes)
-        constraint_force = force.calculate_force_from_displacement_map(norm_mode_shapes, flow, modal_coordinates, self.frequencies, pixels, timestep=0.03)
-        print(constraint_force)
-        pass
+        constraint_force = force.calculate_force_from_displacement_map(norm_mode_shapes, flow, modal_coordinates, self.frequencies, self.pixels, timestep=0.03)
+        return constraint_force
 
 
 if __name__ == "__main__":
     mode_shape_config = {
-        "video_path": "C:\\Users\\md21\\surgical-video-modal-analysis\\videos\\20240430_041703.mp4",
+        "video_path": "C:\\Users\\md21\\surgical-video-modal-analysis\\videos\\heart_beating.mp4",
         "K": 16,
         "fps": 20.0,
         "video_type": "mono",
@@ -104,4 +120,17 @@ if __name__ == "__main__":
         mode_shape_config=mode_shape_config
     )
 
-    estimator._calculate_force(270, 360, (10, 30))
+    constraint_forces = []
+    force_buff = 0
+    for i in range(1, 350):
+        tmp_force = estimator.calculate_force(0, i)
+        force_buff += tmp_force
+        constraint_forces.append(tmp_force)
+    
+    t = (1./30) * np.arange(1, 350, 1)
+
+    constraint_forces = np.array(constraint_forces)
+    fig, axs = plt.subplots(2, sharex=True)
+    axs[0].plot(t, constraint_forces[:, 0])
+    axs[1].plot(t, constraint_forces[:, 1])
+    plt.show()
