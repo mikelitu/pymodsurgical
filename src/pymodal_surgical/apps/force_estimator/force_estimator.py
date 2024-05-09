3c01dc1a0045a50deeb11aa35b002153e15d7934
mikelitu <mikelitubiomed@gmail.com>
2024-05-09 14:35:15 +0100

    fix minor bugs and add documentation for the applications

--- src/pymodal_surgical/apps/force_estimator/force_estimator.py
+++ src/pymodal_surgical/apps/force_estimator/force_estimator.py
@@ -9,6 +9,9 @@ import pymodal_surgical.modal_analysis.force as force
 import pymodal_surgical.modal_analysis.math_helper as math_helper
 import matplotlib.pyplot as plt
 import cv2
+from pymodal_surgical.utils import create_save_dir
+from torchvision.utils import flow_to_image
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
@@ -26,7 +29,22 @@ class ForceEstimator():
         self.mode_shapes = mode_shape_calculator.complex_mode_shapes
         self.frequencies = mode_shape_calculator.frequencies
         self._load_force_video(force_estimation_config)
-    
+
+
+        if "save_force" in force_estimation_config.keys() and force_estimation_config["save_force"] != "":
+            self.save_force = True
+            self.force_save_path = force_estimation_config["save_force"]
+            force_video_path = create_save_dir(self.force_save_path, Path(mode_shape_config["video_path"]).stem)
+            video_name = Path(force_estimation_config["video_path"]).stem + ".mp4"
+            force_video_file = force_video_path /"force"/ video_name
+            force_video_file.parent.mkdir(parents=True, exist_ok=True)
+            width, height = self.force_video_reader.video_width, self.force_video_reader.video_height
+            self.force_video_writer = cv2.VideoWriter(str(force_video_file), cv2.VideoWriter_fourcc(*"mp4v"), force_estimation_config["fps"], (128, 128))
+            self._create_mask(torch.zeros((128, 128)))
+        else:
+            self.save_force = False
+            self.force_save_path = None
+
     def _load_force_video(
         self,
         force_estimation_config: dict
@@ -36,7 +54,65 @@ class ForceEstimator():
         self.flow_model = optical_flow.load_flow_model(device)
         self.pixels = None
 
+
+    def _create_mask(
+        self,
+        frame: np.ndarray,
+    ) -> None:
+        
+        self.force_mask = torch.zeros((2, *frame.shape[:2]), dtype=torch.float32)
     
+
+    def _blend_frame_with_force(
+        self,
+        frame: np.ndarray,
+        force: torch.Tensor,
+    ) -> np.ndarray:
+        
+        # force = force.numpy()
+        self.force_mask[:, self.pixels[0][0]:self.pixels[0][1], self.pixels[1][0]:self.pixels[1][1]] = force
+        print(self.force_mask.max(), self.force_mask.min())
+        # force_mask = cv2.normalize(self.force_mask, None, 0, 255, cv2.NORM_MINMAX)
+        force_mask = flow_to_image(self.force_mask)
+        force_mask = force_mask.permute(1, 2, 0).numpy().astype(np.uint8)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame = cv2.resize(frame, (128, 128))
+        # force = cv2.applyColorMap(force.astype(np.uint8), cv2.COLORMAP_JET)
+        frame = cv2.addWeighted(frame, 0.7, force_mask, 0.3, 0)
+        return frame
+
+    def _blend_quiver_with_frame(
+        self,
+        frame: np.ndarray,
+        force: torch.Tensor,
+    ) -> np.ndarray:
+        
+        X, Y = np.meshgrid(np.arange(0, self.force_mask.shape[2]), np.arange(0, self.force_mask.shape[1]))
+        self.force_mask[:, self.pixels[0][0]:self.pixels[0][1], self.pixels[1][0]:self.pixels[1][1]] = force
+        U = self.force_mask[0].numpy()
+        V = self.force_mask[1].numpy()
+
+        frame = cv2.resize(frame, (128, 128))
+
+        figsize = (frame.shape[1] / 100, frame.shape[0] / 100)
+        fig = plt.figure(figsize=figsize, dpi=100)
+        ax = fig.add_subplot([0, 0, 1, 1])
+        # ax.imshow(frame)
+        ax.quiver(X, Y, U, V, color="black", scale=30)
+        ax.axis("off")
+
+        canvas = FigureCanvas(fig)
+        canvas.draw()
+        width, height = fig.get_size_inches() * fig.get_dpi()
+        image = np.frombuffer(canvas.tostring_rgb(), dtype="uint8").reshape(int(height), int(width), 3)
+        
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = cv2.resize(image, (128, 128))
+        # Release the figure resources (important to avoid memory issues)
+        plt.close(fig)
+
+        return image
+
     def _show_frames(
         self,
         frame: np.ndarray,
@@ -51,6 +127,7 @@ class ForceEstimator():
     ) -> tuple[int, int]:
         
         frame = cv2.resize(frame, (self.mode_shapes.shape[3], self.mode_shapes.shape[2]))
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         bbox = cv2.selectROI("Frame", frame, fromCenter=True)
         cv2.destroyAllWindows()
         return bbox
@@ -59,6 +136,7 @@ class ForceEstimator():
         self,
         idx_1: int,
         idx_2: int,
+        simplify_force: bool = False,
     ) -> torch.Tensor:
         
         # Load the frames
@@ -72,11 +150,11 @@ class ForceEstimator():
             self.pixels = (pix_w, pix_h)
 
         # Preprocess the frames for the calculation of optical flow
-        frame_1 = optical_flow.preprocess_for_raft(frame_1).unsqueeze(0).to(device)
-        frame_2 = optical_flow.preprocess_for_raft(frame_2).unsqueeze(0).to(device)
+        processed_frame_1 = optical_flow.preprocess_for_raft(frame_1).unsqueeze(0).to(device)
+        processed_frame_2 = optical_flow.preprocess_for_raft(frame_2).unsqueeze(0).to(device)
 
         # Calculate the optical flow
-        flow = optical_flow.estimate_flow(self.flow_model, frame_1, frame_2)
+        flow = optical_flow.estimate_flow(self.flow_model, processed_frame_1, processed_frame_2)
         flow = flow.squeeze(0).detach().cpu()
 
         # The optical flow corresponds to the desired displacement of the pixels
@@ -87,13 +165,25 @@ class ForceEstimator():
         # Calculate the constraint force
         modal_coordinates = torch.zeros(self.mode_shapes.shape[0], 2)
         norm_mode_shapes = math_helper.orthonormal_normalization(self.mode_shapes)
-        constraint_force = force.calculate_force_from_displacement_map(norm_mode_shapes, flow, modal_coordinates, self.frequencies, self.pixels, timestep=0.03)
+        constraint_force = force.calculate_force_from_displacement_map(norm_mode_shapes, flow, modal_coordinates, self.frequencies, self.pixels, simplify_force=simplify_force, timestep=0.03)
+        
+        if self.save_force:
+
+            if simplify_force:
+                raise ValueError("Cannot save simplified force to plot as a video grid")
+            
+            blended_force = self._blend_quiver_with_frame(frame_2, constraint_force)
+            self.force_video_writer.write(blended_force)
+
+
         return constraint_force
 
 
 if __name__ == "__main__":
+    plot_force = False
+
     mode_shape_config = {
-        "video_path": "C:\\Users\\md21\\surgical-video-modal-analysis\\videos\\heart_beating.mp4",
+        "video_path": "/home/md21/heart_experiments/real/processed/20240503_094013.mp4",
         "K": 16,
         "fps": 20.0,
         "video_type": "mono",
@@ -101,7 +191,7 @@ if __name__ == "__main__":
         "end": 0,
         "masking": {
             "enabled": True,
-            "mask": "C:\\Users\\md21\\surgical-video-modal-analysis\\videos\\mask\\heart_beating.png"
+            "mask": "/home/md21/heart_experiments/real/processed/mask.png"
         },
         "filtering": {
             "enabled": True,
@@ -109,10 +199,14 @@ if __name__ == "__main__":
             "sigma": 3.0
         }
     }
+
     force_video_config = {
         "fps": 20.0,
         "video_type": "mono",
-        "video_path": "C:/Users/md21/surgical-video-modal-analysis/videos/20240430_041703.mp4",
+        "video_path": "/home/md21/heart_experiments/real/processed/20240503_094013.mp4",
+        "start": 100,
+        "end": 450,
+        "save_force": "results/"
     }
 
     estimator = ForceEstimator(
@@ -120,17 +214,32 @@ if __name__ == "__main__":
         mode_shape_config=mode_shape_config
     )
 
-    constraint_forces = []
-    force_buff = 0
-    for i in range(1, 350):
-        tmp_force = estimator.calculate_force(0, i)
-        force_buff += tmp_force
-        constraint_forces.append(tmp_force)
-    
-    t = (1./30) * np.arange(1, 350, 1)
+    plotting_force = np.zeros((force_video_config["end"] - force_video_config["start"], 2))
 
-    constraint_forces = np.array(constraint_forces)
-    fig, axs = plt.subplots(2, sharex=True)
-    axs[0].plot(t, constraint_forces[:, 0])
-    axs[1].plot(t, constraint_forces[:, 1])
-    plt.show()
+    for i in range(force_video_config["start"], force_video_config["end"]):
+        idx = i - force_video_config["start"]
+        tmp_force = estimator.calculate_force(force_video_config["start"], i, simplify_force=False)
+        # plotting_force[idx] = tmp_force
+        # print(f"Force at frame {i}: {tmp_force}")
+
+    if estimator.save_force:
+        estimator.force_video_writer.release()
+        cv2.destroyAllWindows()
+    
+    if plot_force:
+        fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+        time = (1/force_video_config["fps"]) * np.arange(force_video_config["start"], force_video_config["end"])
+        axs[0].plot(time, plotting_force[:, 0])
+        axs[0].set_title("U-Direction Force")
+        axs[0].set_ylabel("Force (N)")
+        axs[1].plot(time, plotting_force[:, 1])
+        axs[1].set_title("V-Direction Force")
+        axs[1].set_ylabel("Force (N)")
+        fig.supxlabel("Time (s)")
+        plt.show()
+
+    
+    
+
+    
+    
