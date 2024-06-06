bfcea7293fe53febe64c84ebf586251958a91c1a
mikelitu <mikelitubiomed@gmail.com>
2024-06-06 11:48:12 +0100

    clean the force estimator script

--- src/pymodal_surgical/apps/force_estimator/force_estimator.py
+++ src/pymodal_surgical/apps/force_estimator/force_estimator.py
@@ -10,6 +10,7 @@ import pymodal_surgical.modal_analysis.math_helper as math_helper
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import cv2
+from scipy import signal
 from pymodal_surgical.utils import create_save_dir
 from torchvision.utils import flow_to_image
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -43,7 +44,11 @@ class ForceEstimator():
             video_name = Path(force_estimation_config["video_path"]).stem + ".mp4"
             force_video_file = force_video_path /"force"/ video_name
             force_video_file.parent.mkdir(parents=True, exist_ok=True)
-            width, height = self.force_video_reader.video_width, self.force_video_reader.video_height
+            if self.force_video_reader.video_type == "stereo":
+                width, height = self.force_video_reader.video_width // 2, self.force_video_reader.video_height
+            else:
+                width, height = self.force_video_reader.video_width, self.force_video_reader.video_height
+            
             self.force_video_writer = cv2.VideoWriter(str(force_video_file), cv2.VideoWriter_fourcc(*"mp4v"), self.fps, (height, width))
             self._create_mask((height, width))
         
@@ -99,10 +104,10 @@ class ForceEstimator():
     ) -> np.ndarray:
         
         gd = 20
-        X, Y = np.mgrid[0:frame.shape[0]:gd, 0:frame.shape[1]:gd]
+        Y, X = np.mgrid[0:frame.shape[0]:gd, 0:frame.shape[1]:gd]
         self.force_mask[:, self.pixels[0][0]:self.pixels[0][1], self.pixels[1][0]:self.pixels[1][1]] = force
-        U = self.force_mask[0, ::gd, ::gd].numpy()
-        V = self.force_mask[1, ::gd, ::gd].numpy()
+        V = self.force_mask[0, ::gd, ::gd].numpy()
+        U = self.force_mask[1, ::gd, ::gd].numpy()
 
         # frame = cv2.resize(frame, (128, 128))
         # frame_slice = frame[::gd, ::gd]
@@ -112,9 +117,9 @@ class ForceEstimator():
         fig = plt.figure(figsize=figsize, dpi=dpi)
         ax = fig.add_subplot([0, 0, 1, 1])
         ax.imshow(frame, zorder=0, alpha=1.0, interpolation="hermite")
-        rec_limits = (self.pixels[0][0] + 10, self.pixels[0][1] + 10, self.pixels[1][0] + 10, self.pixels[1][1] + 10)
-        rect = patches.Rectangle((rec_limits[0], rec_limits[2]), rec_limits[1] - rec_limits[0], rec_limits[3] - rec_limits[2], linewidth=2, edgecolor="k", facecolor="none")
-        ax.quiver(X, Y, U, V, color="yellow", scale=25)
+        rec_limits = (self.pixels[0][0], self.pixels[0][1], self.pixels[1][0], self.pixels[1][1])
+        rect = patches.Rectangle((rec_limits[0], rec_limits[2]), rec_limits[1] - rec_limits[0], rec_limits[3] - rec_limits[2], linewidth=1.0, edgecolor="k", facecolor="none")
+        ax.quiver(Y, X, V, U, color="yellow", scale=25)
         ax.add_patch(rect)
         ax.axis("off")
 
@@ -158,8 +163,13 @@ class ForceEstimator():
     ) -> torch.Tensor:
         
         # Load the frames
-        frame_1 = self.force_video_reader.read_frame(idx_1)
-        frame_2 = self.force_video_reader.read_frame(idx_2)
+        if self.force_video_reader.video_type == "stereo":
+            frame_1 = self.force_video_reader.read_frame(idx_1)[0]
+            frame_2 = self.force_video_reader.read_frame(idx_2)[0]
+        
+        else:
+            frame_1 = self.force_video_reader.read_frame(idx_1)
+            frame_2 = self.force_video_reader.read_frame(idx_2)
 
         if self.pixels is None:
             roi = self._select_roi(frame_1)
@@ -202,111 +212,3 @@ class ForceEstimator():
             self.force_video_writer.write(blended_force)
 
         return constraint_force
-
-
-if __name__ == "__main__":
-    
-    plot_force = False
-    plot_dict = {}
-    pixels = None
-
-    for K in [16]:
-
-        mode_shape_config = {
-                "video_path": "/home/md21/heart_experiments/phantom/processed/20240501_042319.mp4",
-                "K": K,
-                "video_type": "mono",
-                "start": 0,
-                "end": 120,
-                "batch_size": 2,
-                "masking": {
-                    "enabled": True,
-                    "mask": "/home/md21/heart_experiments/phantom/mask.png"
-                },
-                "filtering": {
-                    "enabled": True,
-                    "size": 15,
-                    "sigma": 2.0
-                },
-
-                "save_mode_shapes": True,
-            }
-
-
-        if K == 4:
-            
-            force_video_config = {
-                "video_type": "mono",
-                "video_path": "/home/md21/heart_experiments/phantom/processed/20240501_033343.mp4",
-                "start": 0,
-                "end": 0,
-
-            }
-        
-        else:
-
-            force_video_config = {
-                "video_type": "mono",
-                "video_path": "/home/md21/heart_experiments/phantom/processed/20240501_033343.mp4",
-                "start": 0,
-                "end": 0,
-                "pixels": pixels,
-                "save_force": "results"
-            }
-
-        estimator = ForceEstimator(
-            force_estimation_config=force_video_config,
-            mode_shape_config=mode_shape_config
-        )
-
-
-        if force_video_config["end"] == 0 or force_video_config["end"] > len(estimator.force_video_reader):
-            force_video_config["end"] = len(estimator.force_video_reader)
-
-        plotting_force = np.zeros((force_video_config["end"] - force_video_config["start"], 2))
-
-        for i in range(force_video_config["start"], force_video_config["end"]):
-            idx = i - force_video_config["start"]
-
-            tmp_force = estimator.calculate_force(force_video_config["start"], i, simplify_force=plot_force)
-            
-            if idx == 0 and K == 4:
-                pixels = estimator.pixels
-
-            if plot_force:
-                plotting_force[idx] = tmp_force / K
-            # print(f"Force at frame {i}: {tmp_force}")
-
-        if estimator.save_force:
-            estimator.force_video_writer.release()
-            cv2.destroyAllWindows()
-        
-        plot_dict[K] = plotting_force
-    
-
-    if plot_force:
-        dpi = 100
-        # plotting_force = (plotting_force - plotting_force.min()) / (plotting_force.max() - plotting_force.min())
-        real_data = np.load("experiments/phantom/2024-05-01_03-34-27/force.npy")
-        real_data = (real_data - real_data.mean(axis=0)) / real_data.std(axis=0)
-        # real_data = (real_data - real_data.min(axis=0)) / (real_data.max(axis=0) - real_data.min(axis=0) + 1e-6)
-        plot_real_data = real_data[1200:]
-        real_time = (1/240) * np.arange(0, plot_real_data.shape[0], 1)
-        plt.rcParams.update({'font.size': 22, 'font.family': 'serif', 'font.serif': 'Times New Roman'})
-        fig, axs = plt.subplots(2, 1, figsize=(1920 / dpi, 1080 / dpi), dpi = dpi, sharex=True)
-        fig.supxlabel("Time (s)", fontweight="bold")
-        fig.supylabel("Force", fontweight="bold")
-        time = (1/estimator.fps) * np.arange(force_video_config["start"], force_video_config["end"])
-        
-        for K, plotting_force in plot_dict.items():
-            # plotting_force = (plotting_force - plotting_force.min(axis=0)) / (plotting_force.max(axis=0) - plotting_force.min(axis=0) + 1e-6)
-            plotting_force = (plotting_force - plotting_force.mean(axis=0)) / plotting_force.std(axis=0)
-            axs[0].plot(time, plotting_force[:, 0], "--", linewidth=3.0, color="#8cc5e3")
-            axs[0].plot(real_time, -plot_real_data[:, 2], linewidth=3.0, color="#f0b077")
-            axs[0].set_ylabel("u")
-            axs[1].plot(time, plotting_force[:, 1], "--", linewidth=3.0, color="#8cc5e3")
-            axs[1].plot(real_time, plot_real_data[:, 1], linewidth=3.0, color="#f0b077")
-            axs[1].set_ylabel("v")
-        
-        fig.legend([f"K={K}" for K in plot_dict.keys()] + ["Force Sensor"], loc="upper right")
-        plt.show()
